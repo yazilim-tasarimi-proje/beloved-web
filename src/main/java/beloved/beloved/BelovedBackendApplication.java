4f6c2cac51b5b78223f3340727a9bf82e4a4db02
zeynepince0 <zynparjn09@gmail.com>
2025-07-04 11:01:33 +0300

    relations

---
+++ src/main/java/beloved/beloved/BelovedBackendApplication.java
@@ -0,0 +1,12 @@
+package beloved.beloved.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelovedBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BelovedBackendApplication.class, args);
+    }
+}
